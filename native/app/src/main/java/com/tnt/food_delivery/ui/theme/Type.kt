392ab0240eacfeda3bd1d804193fcc68812981a5
tientn <20520808@gm.uit.edu.vn>
2023-06-07 21:29:38 +0700

    update font family default

--- native/app/src/main/java/com/tnt/food_delivery/ui/theme/Type.kt
+++ native/app/src/main/java/com/tnt/food_delivery/ui/theme/Type.kt
@@ -8,15 +8,50 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.tnt.food_delivery.R
 
+val Viga = FontFamily(Font(R.font.viga_regular))
+
+val Inter = FontFamily(
+    Font(R.font.inter_thin, weight = FontWeight(100)),
+    Font(R.font.inter_extralight, weight = FontWeight(200)),
+    Font(R.font.inter_light, weight = FontWeight(300)),
+    Font(R.font.inter_regular, weight = FontWeight(400)),
+    Font(R.font.inter_medium, weight = FontWeight(500)),
+    Font(R.font.inter_semibold, weight = FontWeight(600)),
+    Font(R.font.inter_bold, weight = FontWeight(700)),
+    Font(R.font.inter_extrabold, weight = FontWeight(800)),
+    Font(R.font.inter_black, weight = FontWeight(900)),
+)
+
 // Set of Material typography styles to start with
+private val defaultTypography = Typography()
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Inter, //FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+
+    displayLarge = defaultTypography.displayLarge.copy(fontFamily = Inter),
+    displayMedium = defaultTypography.displayMedium.copy(fontFamily = Inter),
+    displaySmall = defaultTypography.displaySmall.copy(fontFamily = Inter),
+
+    headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = Inter),
+    headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = Inter),
+    headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = Inter),
+
+    titleLarge = defaultTypography.titleLarge.copy(fontFamily = Inter),
+    titleMedium = defaultTypography.titleMedium.copy(fontFamily = Inter),
+    titleSmall = defaultTypography.titleSmall.copy(fontFamily = Inter),
+
+    bodyMedium = defaultTypography.bodyMedium.copy(fontFamily = Inter),
+    bodySmall = defaultTypography.bodySmall.copy(fontFamily = Inter),
+
+    labelLarge = defaultTypography.labelLarge.copy(fontFamily = Inter),
+    labelMedium = defaultTypography.labelMedium.copy(fontFamily = Inter),
+    labelSmall = defaultTypography.labelSmall.copy(fontFamily = Inter)
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -33,18 +68,4 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
-)
-
-val Viga = FontFamily(Font(R.font.viga_regular))
-
-val Inter = FontFamily(
-    Font(R.font.inter_thin, weight = FontWeight(100)),
-    Font(R.font.inter_extralight, weight = FontWeight(200)),
-    Font(R.font.inter_light, weight = FontWeight(300)),
-    Font(R.font.inter_regular, weight = FontWeight(400)),
-    Font(R.font.inter_medium, weight = FontWeight(500)),
-    Font(R.font.inter_semibold, weight = FontWeight(600)),
-    Font(R.font.inter_bold, weight = FontWeight(700)),
-    Font(R.font.inter_extrabold, weight = FontWeight(800)),
-    Font(R.font.inter_black, weight = FontWeight(900)),
 )
